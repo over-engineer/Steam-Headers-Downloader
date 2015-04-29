63c2dc71c16921c7ce98b5c9c9239fc6d10c928f
Dinos <dn0zemail@gmail.com>
2015-04-29 14:38:41 +0300

    Update steam_headers_download.py

--- steam_headers_download.py
+++ steam_headers_download.py
@@ -11,7 +11,7 @@
 # 
 # The MIT License (MIT)
 # 
-# Copyright (c) 2014 Dinos
+# Copyright (c) 2015 Dinos
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
